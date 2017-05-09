a3e3c2694bce6b2be5e042d09f832d4c634aaeac
Rafael Souza da Silva <computacao.rafael@gmail.com>
2017-05-09 10:19:02 -0300

    Aula 21: Entendendo o Component Selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers', //Seletor pelo nome
+  //selector: `[app-servers]`, //Seletor por atributo
+  //selector: `.app-servers`, //Seletor por classe
   template: `
     <app-server></app-server>
     <app-server></app-server>
